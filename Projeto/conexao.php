9b813779837c5a0ba504a3356ce346aefed3db18
MateusJ <mateus3048@gmail.com>
2023-05-24 12:53:47 -0300

    conexaoBD

---
+++ Projeto/conexao.php
@@ -0,0 +1,16 @@
+<?php
+
+$servidor = "localhost"; //variável responsável por guardar o nome do  servidor
+$user = "root"; //variável responsável por guardar o nome de usuário do banco
+$senha = "0312mateus"; //variável responsável por guardar a senha de acesso ao banco
+$banco = "orgprojeto"; //variável responsável por guardar o nome do banco de dados
+
+$mysqli = new mysqli($servidor, $user,$senha,$banco);
+
+if ($mysqli -> connect_error){
+    die("connection failed: ".mysqli_connect_error());
+}else{
+    echo "tudo certo";
+}
+
+?>
